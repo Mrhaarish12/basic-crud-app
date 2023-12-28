8ffac477a2adc0f5b12197f5a6f787803e904d98
Haarish Khan <haarish@werqlabs.com>
2023-12-29 03:32:56 +0530

    program.cs file

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,7 +1,10 @@
+using FirstProject.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCDemoDbContext>(options =>
+);
 
 var app = builder.Build();
 

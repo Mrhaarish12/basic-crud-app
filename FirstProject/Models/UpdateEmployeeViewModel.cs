d079cc13a0dd81b2347e0d69058a327ddaedf435
Haarish Khan <haarish@werqlabs.com>
2023-12-31 23:45:24 +0530

    employee

--- FirstProject/Models/UpdateEmployeeViewModel.cs
+++ FirstProject/Models/UpdateEmployeeViewModel.cs
@@ -3,6 +3,7 @@
     public class UpdateEmployeeViewModel
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }    
+        public string Name { get; set; }   
+        public string Email { get; set; }
     }
 }

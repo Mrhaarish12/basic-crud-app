f33cbf4985e6831801b5753a168a2e77b62b9a5c
Haarish Khan <haarish@werqlabs.com>
2023-12-31 17:04:51 +0530

    addemployeemodel properties

--- FirstProject/Models/AddEmployeeViewModelcs.cs
+++ FirstProject/Models/AddEmployeeViewModelcs.cs
@@ -2,5 +2,11 @@
 {
     public class AddEmployeeViewModelcs
     {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+        public long Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
     }
 }

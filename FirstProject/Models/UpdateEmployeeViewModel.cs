e23fa1b7ebb3bb2a403563c488dafe3d2a034058
Haarish Khan <haarish@werqlabs.com>
2023-12-31 23:47:16 +0530

    employee

--- FirstProject/Models/UpdateEmployeeViewModel.cs
+++ FirstProject/Models/UpdateEmployeeViewModel.cs
@@ -7,5 +7,6 @@
         public string Email { get; set; }
         public long Salary { get; set; }
         public DateTime DateOfBirth { get; set;}
+        public string Department { get; set; }
     }
 }
